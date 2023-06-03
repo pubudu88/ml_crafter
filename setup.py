e9c66e143b8a0445e67809ef34b6ed44a0f86e68
HOMEPC\ruk01 <pubudurukmal88@gmail.com>
2023-06-03 06:28:47 +0100

    updated git ignore

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='ml-crafter',
+    version='0.1.0',
+    description='Performs end to end ML model development',
+    packages=['ml_crafter'],
+    install_requires=[
+        # List your package dependencies here
+    ],
+)
